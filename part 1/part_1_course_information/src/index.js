aa76e04bc06f24b76dd01a1779ff9e46921669b4
ino-bar <50051423+ino-bar@users.noreply.github.com>
2020-07-10 15:31:44 +0100

    10-07-20 2 part 1.2 completed

--- part 1/part_1_course_information/src/index.js
+++ part 1/part_1_course_information/src/index.js
@@ -7,10 +7,20 @@ const Header = (props) => {
     )
 };
 
+const Part = (props) => {
+    return (
+            <p>{props.part} {props.exercise}</p>
+        )
+}
+
 const Content = (props) => {
     return (
-        <p>{ props.part } { props.exercise }</p>
-    )
+        <>
+        <Part part={props.part1} exercise={props.exercise1} />
+        <Part part={props.part2} exercise={props.exercise2} />
+        <Part part={props.part3} exercise={props.exercise3} />
+        </>
+        )
 };
 
 const Total = (props) => {
@@ -31,9 +41,7 @@ const App = () => {
     return (
         <div>
             <Header courseTitle={course} />
-            <Content part={part1} exercise={exercises1} />
-            <Content part={part2} exercise={exercises2} />
-            <Content part={part3} exercise={exercises3} />
+            <Content part1={part1} exercise1={exercises1} part2={part2} exercise2={exercises2} part3={part3} exercise3={exercises3} />
             <Total exercise1={exercises1} exercise2={exercises2} exercise3={exercises3} /> 
         </div>
     )
