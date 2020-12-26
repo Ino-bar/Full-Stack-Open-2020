d5d077e6fdd461ef01b01af6c72f15967bd10f4a
ino-bar <ino-bar@users.noreply.github.com>
2020-12-26 22:50:22 +0000

    26-12-20 map anecdotes

--- part 1/part_1_anecdotes/src/index.js
+++ part 1/part_1_anecdotes/src/index.js
@@ -29,12 +29,21 @@ const App = () => {
 
     return (
         <div>
+            <Anecdote anecdote={anecdotes[selected]} />
             <Button onClick={getRandomAnecdote} text ="next anecdote"/>
-            <Anecdote anecdote={anecdotes[selected]}/>
         </div>
         )
 }
 
+const anecdotes = new.map()
+anecdotes.set(0, { anecdote: "If it hurts, do it more often", votes: 0 })
+anecdotes.set(0, { anecdote: "Adding manpower to a late software project makes it later!", votes: 0 })
+anecdotes.set(0, { anecdote: "The first 90 percent of the code accounts for the first 90 percent of the development time...The remaining 10 percent of the code accounts for the other 90 percent of the development time.", votes: 0 })
+anecdotes.set(0, { anecdote: "Any fool can write code that a computer can understand. Good programmers write code that humans can understand.", votes: 0 })
+anecdotes.set(0, { anecdote: "Premature optimization is the root of all evil.", votes: 0 })
+anecdotes.set(0, { anecdote: "Debugging is twice as hard as writing the code in the first place.Therefore, if you write the code as cleverly as possible, you are, by definition, not smart enough to debug it.", votes: 0 })
+
+/*
 const anecdotes = [
     'If it hurts, do it more often',
     'Adding manpower to a late software project makes it later!',
@@ -43,7 +52,7 @@ const anecdotes = [
     'Premature optimization is the root of all evil.',
     'Debugging is twice as hard as writing the code in the first place. Therefore, if you write the code as cleverly as possible, you are, by definition, not smart enough to debug it.'
 ]
-
+*/
 ReactDOM.render(
   <React.StrictMode>
     <App />
