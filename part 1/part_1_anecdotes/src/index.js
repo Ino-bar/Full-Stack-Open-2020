750d63095fa47f58a56abb89514239ab032e4f88
ino-bar <ino-bar@users.noreply.github.com>
2020-12-27 17:42:54 +0000

    27-12-20 anecdotes updates

--- part 1/part_1_anecdotes/src/index.js
+++ part 1/part_1_anecdotes/src/index.js
@@ -3,6 +3,8 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import reportWebVitals from './reportWebVitals';
 
+
+var randInt = 0
 const Button = ({ onClick, text }) => (
     <button onClick={onClick}>
         {text}
@@ -13,35 +15,48 @@ const Anecdote = (props) => (
     <div>{props.anecdote}</div>
     )
 
+const Votes = (props) => (
+    <div>has {props.vote} votes</div>
+)
+
 function getRandomInRange(min, max) {
     min = Math.ceil(min)
     max = Math.floor(max)
     var randNumber = Math.floor(Math.random() * (max - min) + min)
+    while (randInt === randNumber) {
+        getRandomInRange(min, max)
+    }
+    randInt = randNumber
     return randNumber
 }
+const anecdotes = new Map()
+anecdotes.set(0, { anecdote: "If it hurts, do it more often", votes: 0 })
+anecdotes.set(1, { anecdote: "Adding manpower to a late software project makes it later!", votes: 0 })
+anecdotes.set(2, { anecdote: "The first 90 percent of the code accounts for the first 90 percent of the development time...The remaining 10 percent of the code accounts for the other 90 percent of the development time.", votes: 0 })
+anecdotes.set(3, { anecdote: "Any fool can write code that a computer can understand. Good programmers write code that humans can understand.", votes: 0 })
+anecdotes.set(4, { anecdote: "Premature optimization is the root of all evil.", votes: 0 })
+anecdotes.set(5, { anecdote: "Debugging is twice as hard as writing the code in the first place.Therefore, if you write the code as cleverly as possible, you are, by definition, not smart enough to debug it.", votes: 0 })
 
 const App = () => {
-    const [selected, setSelected] = useState(0)
+    //const [selected, setSelected] = useState(0)
+    const [randomAnecdote, setRandomAnecdote] = useState(anecdotes.get(getRandomInRange(0, anecdotes.size)))
+    const [vote, setVote] = useState(0)
 
-    const getRandomAnecdote = () => {
-        setSelected(getRandomInRange(0, anecdotes.length))
+    const getRandomAnecdote = () => /*getRandomInRange(0, anecdotes.size)*/setRandomAnecdote(anecdotes.get(getRandomInRange(0, anecdotes.size)))
+
+    const voteForAnecdote = () => {
+        randomAnecdote.votes += 1
+        setVote(randomAnecdote.votes)
     }
-
     return (
         <div>
-            <Anecdote anecdote={anecdotes[selected]} />
-            <Button onClick={getRandomAnecdote} text ="next anecdote"/>
+            <Anecdote anecdote={randomAnecdote.anecdote} />
+            <Votes vote={randomAnecdote.votes} />
+            <Button onClick={getRandomAnecdote} text="next anecdote" />
+            <Button onClick={voteForAnecdote} text="vote" />
         </div>
         )
 }
-
-const anecdotes = new.map()
-anecdotes.set(0, { anecdote: "If it hurts, do it more often", votes: 0 })
-anecdotes.set(0, { anecdote: "Adding manpower to a late software project makes it later!", votes: 0 })
-anecdotes.set(0, { anecdote: "The first 90 percent of the code accounts for the first 90 percent of the development time...The remaining 10 percent of the code accounts for the other 90 percent of the development time.", votes: 0 })
-anecdotes.set(0, { anecdote: "Any fool can write code that a computer can understand. Good programmers write code that humans can understand.", votes: 0 })
-anecdotes.set(0, { anecdote: "Premature optimization is the root of all evil.", votes: 0 })
-anecdotes.set(0, { anecdote: "Debugging is twice as hard as writing the code in the first place.Therefore, if you write the code as cleverly as possible, you are, by definition, not smart enough to debug it.", votes: 0 })
 
 /*
 const anecdotes = [
