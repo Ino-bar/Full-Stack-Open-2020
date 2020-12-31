bf37c8511921801338c568cce7568d88476f4ea1
ino-bar <ino-bar@users.noreply.github.com>
2020-12-31 15:07:20 +0000

    31-12-20 1.14 anecdotes step 3 completed

--- part 1/part_1_anecdotes/src/index.js
+++ part 1/part_1_anecdotes/src/index.js
@@ -3,7 +3,6 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import reportWebVitals from './reportWebVitals';
 
-
 var randInt = 0
 var selected = 0
 
@@ -31,17 +30,19 @@ function getRandomInRange(min, max) {
     randInt = randNumber
     return randNumber
 }
+
 const anecdotes = new Map()
-anecdotes.set(0, { anecdote: "If it hurts, do it more often", votes: 0 })
+anecdotes.set(0, { anecdote: "If it hurts, do it more often.", votes: 0 })
 anecdotes.set(1, { anecdote: "Adding manpower to a late software project makes it later!", votes: 0 })
 anecdotes.set(2, { anecdote: "The first 90 percent of the code accounts for the first 90 percent of the development time...The remaining 10 percent of the code accounts for the other 90 percent of the development time.", votes: 0 })
 anecdotes.set(3, { anecdote: "Any fool can write code that a computer can understand. Good programmers write code that humans can understand.", votes: 0 })
 anecdotes.set(4, { anecdote: "Premature optimization is the root of all evil.", votes: 0 })
-anecdotes.set(5, { anecdote: "Debugging is twice as hard as writing the code in the first place.Therefore, if you write the code as cleverly as possible, you are, by definition, not smart enough to debug it.", votes: 0 })
+anecdotes.set(5, { anecdote: "Debugging is twice as hard as writing the code in the first place. Therefore, if you write the code as cleverly as possible, you are, by definition, not smart enough to debug it.", votes: 0 })
 
 const App = () => {
     const [state, setState] = useState(anecdotes)
     const [randomAnecdote, setRandomAnecdote] = useState(anecdotes.get(selected))
+    const [mostVotedAnecdote, setMostVoted] = useState(anecdotes.get(selected))
 
     const upsert = (key, value) => {
         const newMap = new Map(state)
@@ -60,26 +61,37 @@ const App = () => {
         var randanecdotevote = randomAnecdote.votes
         upsert(selected, { randanecdote, randanecdotevote})
     }
+
+    function SetMostVotedAnecdote() {
+        var mostVoted = 0
+        var topAnecdote = mostVotedAnecdote
+        state.forEach((item) => {
+            if (item.votes > mostVoted) {
+                mostVoted = item.votes
+                topAnecdote = item
+            }
+        })
+        setMostVoted(topAnecdote)
+        return (
+            <>
+            <h2>Anecdote with Most Votes</h2>
+                <div>{mostVotedAnecdote.anecdote}</div>
+            </>
+        )
+    }
+
     return (
         <div>
+            <h2>Anecdote of the Day</h2>
             <Anecdote anecdote={randomAnecdote.anecdote} />
             <Votes vote={randomAnecdote.votes} />
             <Button onClick={getRandomAnecdote} text="next anecdote" />
             <Button onClick={voteForAnecdote} text="vote" />
+            <SetMostVotedAnecdote />
         </div>
         )
 }
 
-/*
-const anecdotes = [
-    'If it hurts, do it more often',
-    'Adding manpower to a late software project makes it later!',
-    'The first 90 percent of the code accounts for the first 90 percent of the development time...The remaining 10 percent of the code accounts for the other 90 percent of the development time.',
-    'Any fool can write code that a computer can understand. Good programmers write code that humans can understand.',
-    'Premature optimization is the root of all evil.',
-    'Debugging is twice as hard as writing the code in the first place. Therefore, if you write the code as cleverly as possible, you are, by definition, not smart enough to debug it.'
-]
-*/
 ReactDOM.render(
     <App />,
   document.getElementById('root')
